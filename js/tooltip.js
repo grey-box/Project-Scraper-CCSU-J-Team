714651f40ee68010d8effb19fc409b1a45babd63
D-anijel-S-tefanovic <danijel.stefanovic@mohawkcollege.ca>
2023-09-04 13:58:14 -0400

    Updates

---
+++ js/tooltip.js
@@ -0,0 +1,7 @@
+  // Initialize tooltip component
+  document.addEventListener("DOMContentLoaded", function(){
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  });
